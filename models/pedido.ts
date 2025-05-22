49d902a2c2db3f46a73933da3c1ce8b1dbe0f042
eddynorris <eddy.sagitario.97@gmail.com>
2025-05-22 16:53:08 -0500

    Preparando para deploy de apk

--- models/pedido.ts
+++ models/pedido.ts
@@ -5,11 +5,11 @@ import { UserSimple } from './user';
 import { PedidoDetalle } from './pedidoDetalle';
 
 // Tipos posibles para el estado del pedido (Movido aquí)
-export const ESTADOS_PEDIDO: Array<'programado' | 'confirmado' | 'entregado' | 'cancelado'> = [
+export const ESTADOS_PEDIDO: Array<'programado' | 'confirmado'> = [
   'programado',
-  'confirmado',
-  'entregado',
-  'cancelado'
+  'confirmado'
+  //'entregado',
+  //'cancelado'
 ];
 
 export interface Pedido {
